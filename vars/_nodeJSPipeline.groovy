6f53654a0b5bad2d4c46d59915dcde5642f8af20
Tim Doyle <coder.td@gmail.com>
2019-11-30 20:11:25 -0500

    Adding nodejs pipeline to vars.

---
+++ vars/_nodeJSPipeline.groovy
@@ -0,0 +1,34 @@
+/* Node Js pipeline for building node apps. */
+
+def call(body) {
+
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent none
+        options {
+            buildDiscarder(logRotator(daysToKeepStr: '1', numToKeepStr: '10'))
+            disableConcurrentBuilds()
+        }
+        environment {
+            TEST = "${body.test}"
+        }
+        stages {
+            stage('Setup') {
+                agent {
+                    docker {
+                        image "alpine/git"
+                    }
+                }
+                steps {
+                    script {
+                        println "Setting up!"
+                    }
+                }
+            }
+        }
+    }
+}
