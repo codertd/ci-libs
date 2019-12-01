b283115d445649d56171b136c9256b6b5d088d84
Tim Doyle <coder.td@gmail.com>
2019-12-01 17:31:12 -0500

    image should be node

--- vars/_nodeJSPipeline.groovy
+++ vars/_nodeJSPipeline.groovy
@@ -43,7 +43,7 @@ def call(body) {
             stage('Npm Setup') {
                 agent {
                     docker {
-                        image "docker-node/10"
+                        image "node"
                         args '--entrypoint='
                     }
                 }
